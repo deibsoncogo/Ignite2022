a1ce6003a91647b4fb2407076b6bf69bab6d6f05
Deibson Cogo <deibsoncogo@outlook.com>
2023-01-20 09:47:24 -0300

    ✨ feat p1: criação do componente sidebar

--- ReactJS/Projeto01/src/App.jsx
+++ ReactJS/Projeto01/src/App.jsx
@@ -1,21 +1,29 @@
-import { Header } from './components/Header'
-import { Post } from './Post'
-import './styles.css'
+import styles from './App.module.css';
+import { Header } from './components/Header';
+import { Sidebar } from './components/Sidebar';
+import { Post } from './Post';
+import './styles.css';
 
 export function App() {
   return (
     <>
       <Header />
 
-      <Post
-        author="Tomás Arthur Real"
-        content="Muito bom dever de casa, ótimo"
-      />
+      <div className={styles.wrapper}>
+        <Sidebar />
 
-      <Post
-        author="Renan Noah Cauê Aragão"
-        content="Sou corretor de imóveis no litoral, promovendo a camada de investimento"
-      />
+        <main>
+          <Post
+            author="Tomás Arthur Real"
+            content="Muito bom dever de casa, ótimo"
+          />
+
+          <Post
+            author="Renan Noah Cauê Aragão"
+            content="Sou corretor de imóveis no litoral, promovendo a camada de investimento"
+          />
+        </main>
+      </div>
     </>
   )
 }
