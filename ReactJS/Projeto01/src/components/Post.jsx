2ecb6928583db88f7422edcef53e8d0b72f2f226
Deibson Cogo <deibsoncogo@outlook.com>
2023-01-20 17:35:38 -0300

    🚧 feat p1: implementação parcial da criação de comentários

--- ReactJS/Projeto01/src/components/Post.jsx
+++ ReactJS/Projeto01/src/components/Post.jsx
@@ -1,10 +1,13 @@
 import { format, formatDistanceToNow } from 'date-fns'
 import ptBR from 'date-fns/locale/pt-BR'
+import { useState } from 'react'
 import { Avatar } from './Avatar'
 import { Comment } from './Comment'
 import styles from './Post.module.css'
 
 export function Post({ author, publishedAt, content }) {
+  const [comments, setComments] = useState([1, 2])
+
   const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
     locale: ptBR,
   })
@@ -13,6 +16,11 @@ export function Post({ author, publishedAt, content }) {
     locale: ptBR,
     addSuffix: true,
   })
+
+  function handleCrateNewComment() {
+    event.preventDefault()
+    setComments([...comments, comments.length + 1])
+  }
 
   return (
     <article className={styles.post}>
@@ -40,7 +48,7 @@ export function Post({ author, publishedAt, content }) {
           }
         })}
 
-        <form className={styles.commentForm}>
+        <form onSubmit={handleCrateNewComment} className={styles.commentForm}>
           <strong>Deixe seu feedback</strong>
           <textarea placeholder="Deixe um comentário" />
 
@@ -50,9 +58,9 @@ export function Post({ author, publishedAt, content }) {
         </form>
 
         <div className={styles.commentList}>
-          <Comment />
-          <Comment />
-          <Comment />
+          {comments.map((comment) => {
+            return <Comment />
+          })}
         </div>
       </div>
     </article>
