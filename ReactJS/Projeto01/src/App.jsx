67aa86b4f11976f1363a78b05844fea3622e1856
Deibson Cogo <deibsoncogo@outlook.com>
2023-01-20 10:04:40 -0300

    ✨ feat p1: criação do componente post

--- ReactJS/Projeto01/src/App.jsx
+++ ReactJS/Projeto01/src/App.jsx
@@ -1,7 +1,7 @@
 import styles from './App.module.css';
 import { Header } from './components/Header';
+import { Post } from './components/Post';
 import { Sidebar } from './components/Sidebar';
-import { Post } from './Post';
 import './styles.css';
 
 export function App() {
@@ -13,15 +13,8 @@ export function App() {
         <Sidebar />
 
         <main>
-          <Post
-            author="Tomás Arthur Real"
-            content="Muito bom dever de casa, ótimo"
-          />
-
-          <Post
-            author="Renan Noah Cauê Aragão"
-            content="Sou corretor de imóveis no litoral, promovendo a camada de investimento"
-          />
+          <Post />
+          <Post />
         </main>
       </div>
     </>
