31eb96a7ac9f86b9cf2aae26080a410f5669870d
Deibson Cogo <deibsoncogo@outlook.com>
2023-01-20 08:18:31 -0300

    ✨ feat p1: criação de uma propriedade para componente

--- ReactJS/Projeto01/src/App.jsx
+++ ReactJS/Projeto01/src/App.jsx
@@ -4,7 +4,16 @@ export function App() {
   return (
     <>
       <h1>Hello word</h1>
-      <Post />
+
+      <Post
+        author="Tomás Arthur Real"
+        content="Muito bom dever de casa, ótimo"
+      />
+
+      <Post
+        author="Renan Noah Cauê Aragão"
+        content="Sou corretor de imóveis no litoral, promovendo a camada de investimento"
+      />
     </>
   )
 }
