c72ead4efae2090c9e83c70d4fd85cd5188c0275
Deibson Cogo <deibsoncogo@outlook.com>
2023-01-20 22:24:56 -0300

    🚀 feat p2: implementado o padronizador de rotas

--- ReactJS/Projeto02/src/Router.tsx
+++ ReactJS/Projeto02/src/Router.tsx
@@ -1,3 +1,4 @@
+import { DefaultLayout } from './layouts/DefaultLayout'
 import { Route, Routes } from 'react-router-dom'
 import { History } from './pages/History'
 import { Home } from './pages/Home'
@@ -5,8 +6,10 @@ import { Home } from './pages/Home'
 export function Router() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/history" element={<History />} />
+      <Route path="/" element={<DefaultLayout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/history" element={<History />} />
+      </Route>
     </Routes>
   )
 }
