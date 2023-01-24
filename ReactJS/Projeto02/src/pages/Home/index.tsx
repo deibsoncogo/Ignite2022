6a13a145701517e6c5970e73e91eb2c62b52ffb9
Deibson Cogo <deibsoncogo@outlook.com>
2023-01-24 11:58:23 -0300

    ✨ fix p2: modificação da mensagem de erro da validação do form

--- ReactJS/Projeto02/src/pages/Home/index.tsx
+++ ReactJS/Projeto02/src/pages/Home/index.tsx
@@ -10,7 +10,7 @@ import * as S from './styles'
 
 const newCycleFormValidationSchema = zod.object({
   task: zod.string().min(1, 'Informe a tarefa'),
-  minutesAmount: zod.number().min(5, 'O ciclo precisa ser de no mínimo 5 minutos.').max(60, 'O ciclo precisa ser de no máximo 60 minutos.'),
+  minutesAmount: zod.number().min(5, 'Tempo mínimo de 5 minutos').max(60, 'Tempo máximo de 60 minutos'),
 })
 
 type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
