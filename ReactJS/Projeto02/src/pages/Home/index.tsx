fb486a497199340efa824b0b1a16cfbfc579940c
Deibson Cogo <deibsoncogo@outlook.com>
2023-01-23 22:30:47 -0300

    🚀 feat p2: implementação da validação de formulário

--- ReactJS/Projeto02/src/pages/Home/index.tsx
+++ ReactJS/Projeto02/src/pages/Home/index.tsx
@@ -1,5 +1,7 @@
+import { zodResolver } from '@hookform/resolvers/zod'
 import { Play } from 'phosphor-react'
 import { useForm } from 'react-hook-form'
+import * as zod from 'zod'
 import {
   CountdownContainer,
   FormContainer,
@@ -10,8 +12,18 @@ import {
   TaskInput,
 } from './styles'
 
+const newCycleFormValidationSchema = zod.object({
+  task: zod.string().min(1, 'Informe a tarefa'),
+  minutesAmount: zod
+    .number()
+    .min(5, 'Tempo mínimo de 5 minutos')
+    .max(60, 'Tempo máximo de 60 minutos'),
+})
+
 export function Home() {
-  const { register, handleSubmit, watch } = useForm()
+  const { register, handleSubmit, watch, formState } = useForm({
+    resolver: zodResolver(newCycleFormValidationSchema),
+  })
 
   function handleCreateNewCycle(data: any) {
     console.log(data)
@@ -19,6 +31,10 @@ export function Home() {
 
   const task = watch('task')
   const isSubmitDisable = !task
+
+  if (Object.values(formState.errors).length > 0) {
+    console.error('Error =>', formState.errors)
+  }
 
   return (
     <HomeContainer>
